fd7961e3361efb1c151654ff6d1557e5243b2280
Janek Dijkstra <janek@foolsparadise.de>
2025-08-13 20:01:55 +0200

    add readme & dont show hyprland update notes

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -163,6 +163,8 @@
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
+
+      "ecosystem:no_update_news" = true;
     };
   };
 }
